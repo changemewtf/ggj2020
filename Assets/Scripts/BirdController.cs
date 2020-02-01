fbc2abb7c0f1da19f0b7c99facd016f75f94dad5
Max Cantor <max@maxcantor.net>
2020-01-31 22:52:06 -0500

    Multiple fruits are arranged in MainTestScene, and the BirdController moves bird forward at a constant rate.

--- Assets/Scripts/BirdController.cs
+++ Assets/Scripts/BirdController.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class BirdController : MonoBehaviour
 {
-    public float thrust = 1.0f;
+    public float FlapThrust = 30.0f;
+    public float ForwardThrust = 20.0f;
 
+    private bool flapping;
     private Rigidbody rb;
 
     // Start is called before the first frame update
@@ -13,15 +15,30 @@ public class BirdController : MonoBehaviour
     {
         Debug.Log("Hi I'm a birt");
         rb = GetComponent<Rigidbody>();
+        flapping = false;
+    }
+
+    private void Update()
+    {
+        // In Unity documentation, Input goes in FixedUpdate(), but we're going
+        // to put it in Update() here because we don't want to drop any inputs.
+        // If it's in FixedUpdate(), then slower machines might miss some inputs
+        // because they do not have the power to calculate as many ticks.
+        if (Input.GetKeyDown("space"))
+        {
+            flapping = true;
+        }
     }
 
     // Physics updates go in FixedUpdate() instead of Update()!
     void FixedUpdate()
     {
-        if (Input.GetKeyDown("space"))
-        {
-            Debug.Log("SPACE BAR DEPRESSED!");
-            rb.AddForce(transform.up * thrust, ForceMode.Impulse);
+
+        rb.AddForce(transform.forward * ForwardThrust);
+
+        if (flapping) {
+            rb.AddForce(transform.up * FlapThrust, ForceMode.Impulse);
+            flapping = false;
         }
     }
 }
