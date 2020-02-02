740d186434efbd749b5654dbc75e46fbd05cf9d9
Max Cantor <max@maxcantor.net>
2020-02-02 13:14:59 -0500

    camera tweaks and poop frequency fix again

--- Assets/Scripts/PoopGenerator.cs
+++ Assets/Scripts/PoopGenerator.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class PoopGenerator : MonoBehaviour
 {
     public Transform bird_transform;
+    public bool random;
     public GameObject poop;
 
     // Start is called before the first frame update
     void Start()
     {
-            bird_transform = gameObject.transform;
-
+        bird_transform = gameObject.transform;
     }
 
     // Update is called once per frame
@@ -19,14 +19,13 @@ public class PoopGenerator : MonoBehaviour
     {
         //TODO: change this to allow both auto poop based on 
         float rgn_poop = Random.Range(0.0f, 100.0f);
-        if(rgn_poop < 1.0f) {
+        if (random && rgn_poop < 1.0f)
+        {
             MakePoop();
         }
-
     }
 
     public void MakePoop() {
         Instantiate(poop, new Vector3(bird_transform.position.x, bird_transform.position.y - 300.0f, bird_transform.position.z), Quaternion.identity);
     }
-
 }
